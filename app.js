a7bf4a566c57ef3609cda423384900d688a141c9
Christopher Fleetwood <fleetwoodpersonal@gmail.com>
2020-01-30 19:57:02 +0000

    DONE: PERFORM GET REQUEST FOR TOP ARTISTS TODO: CLEAN UP CODE

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -17,11 +16,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/frontend')));
+app.use('/fa', express.static(__dirname + '/node_modules/font-awesome/css'));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -34,8 +33,7 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(500).send('Something broke!');
 });
 
 console.log('server running at http://localhost:3000')
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.sendFile(__dirname.slice(0, __dirname.length - 6) + 'views/index.html');
